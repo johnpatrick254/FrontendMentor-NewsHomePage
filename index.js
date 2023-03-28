58413dcb64df914544d43595588d9cf090f0350c
John Patrick <jpattrick538@gmail.com>
2023-03-28 18:38:56 +0300

    added media queries and css grid

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function navActive(event){
-    console.log(event.target);
+let closed = false
+const menuClick = () => {
+    const button = document.getElementById("button");
+    const modal =document.getElementById("modal")
+    button.setAttribute("src", "./assets/images/icon-menu" + (closed ? "" : "-close") + ".svg")
+    const menu = document.getElementById("menu")
+    !closed ? modal.style.display ="flex": modal.style.display ="none"
+    console.log(button);
+    menu.style.transition="transform 2.5s linear"
+    !closed ? menu.style.transform="translateX(0)": menu.style.transform="translateX(100%)"
+    closed = !closed
 }
