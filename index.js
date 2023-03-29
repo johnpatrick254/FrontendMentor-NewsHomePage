a0322d3539778e82866481a40ef99d0ca163847b
John Patrick <jpattrick538@gmail.com>
2023-03-29 09:52:56 +0300

    refactored grid layout on desktop and added transition to mobile navbar

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-let closed = false
-const menuClick = () => {
-    const button = document.getElementById("button");
-    const modal =document.getElementById("modal")
-    button.setAttribute("src", "./assets/images/icon-menu" + (closed ? "" : "-close") + ".svg")
-    const menu = document.getElementById("menu")
-    !closed ? modal.style.display ="flex": modal.style.display ="none"
-    console.log(button);
-    menu.style.transition="transform 2.5s linear"
-    !closed ? menu.style.transform="translateX(0)": menu.style.transform="translateX(100%)"
-    closed = !closed
-}
+   const button = document.getElementById("button");
+   const close = document.getElementById("close");
+    const modal =document.getElementById("modal");
+    const menu = document.getElementById("menu");
+
+
+button.addEventListener("click", ()=>{
+    setTimeout(()=>{
+        modal.style.opacity ="1"
+             menu.classList.add("active")
+         button.style.display="none";
+         close.style.display ="flex"
+         
+             
+         },0)
+    })
+
+    close.addEventListener("click", ()=>{
+        menu.classList.remove("active")
+        modal.style.opacity ="0"
+         setTimeout(()=>{
+            button.style.display="flex";
+            close.style.display ="none"
+             
+         },0)
+    })
+
